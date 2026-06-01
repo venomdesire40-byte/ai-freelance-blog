6c0659952d7952fb192476c5438684c898047389
Zaid Khalid <venomdesire40@gmail.com>
2026-06-01 17:23:00 +0500

    revert next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-  optimizePackageImports: ['react', 'react-dom'],
-},
+  /* config options here */
 };
 
 export default nextConfig;
